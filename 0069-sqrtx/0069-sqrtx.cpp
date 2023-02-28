915346e0318e2f62cda082a7d8dfcd01cbf7d861
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-02-28 22:30:55 +0530

    Time: 3 ms (68.20%), Space: 6 MB (8.27%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long long ans=pow(x,0.5);
-        return ans;
+        if (x == 0)
+            return x;
+        int s = 1, e = x;
+        while (s <= e) {
+            long long mid = s + (e - s) / 2;
+            if (mid*mid  == x )
+                return mid;
+            else if (mid*mid > x) {
+                e = mid - 1;
+            }
+            else {
+                s = mid + 1;
+            }
+        }
+        return e;
     }
     
 };
