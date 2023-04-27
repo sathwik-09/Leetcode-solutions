8a8fe98c4cf59a79737acb343dafba142ec2a05d
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-04-27 22:39:11 +0530

    Time: 3 ms (45.34%), Space: 6.1 MB (9.91%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        return sqrt(n);
+        long long i=1;
+        int count=0;
+        while((i*i)<=n){
+            i++;
+            count++;
+        }
+        return count;
     }
 };
