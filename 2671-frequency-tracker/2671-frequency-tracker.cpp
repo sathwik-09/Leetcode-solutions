75e8a9c0e4457cf2c1d70ba0afba825b7a8aee55
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-05-12 21:35:26 +0530

    Time: 495 ms (87.92%), Space: 200.1 MB (79.95%) - LeetHub

---
+++ 2671-frequency-tracker/2671-frequency-tracker.cpp
@@ -0,0 +1,34 @@
+class FrequencyTracker {
+public:
+    unordered_map<int, int> mp;   //store number & frequency count
+    unordered_map<int, int> mps;  //store frequency & count number whose frequecny is same
+    FrequencyTracker() {   
+    }
+    void add(int number) {
+        if(mp.find(number) != mp.end()){
+            mps[mp[number]]--;
+        }
+        mp[number]++;
+        mps[mp[number]]++;
+    }
+    
+    void deleteOne(int number) {
+        if(mp.find(number) != mp.end() && mp[number] > 0){
+            mps[mp[number]]--;
+            mp[number]--;
+            mps[mp[number]]++;
+        }
+    }
+    
+    bool hasFrequency(int frequency) {
+        return (mps[frequency] > 0);
+    }
+};
+
+/**
+ * Your FrequencyTracker object will be instantiated and called as such:
+ * FrequencyTracker* obj = new FrequencyTracker();
+ * obj->add(number);
+ * obj->deleteOne(number);
+ * bool param_3 = obj->hasFrequency(frequency);
+ */
