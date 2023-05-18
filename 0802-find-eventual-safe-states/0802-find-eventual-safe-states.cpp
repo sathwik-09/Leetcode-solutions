47bb6291e8972f23639f007688915ea5536d119b
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-05-18 12:14:00 +0530

    Time: 236 ms (16.52%), Space: 59 MB (34.45%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,44 +1,32 @@
 class Solution {
 public:
-  bool dfs(int node,vector<int> &path,vector<int> &visited,vector<int> &check,vector<int> adj[]){
-      visited[node]=1;
-      path[node]=1;
-      for(auto i:adj[node]){
-          if(visited[i]==0 && path[i]==0){
-              if(dfs(i,path,visited,check,adj)==true){
-                  return true;
-              }
-          }
-          else if(visited[i]==1 && path[i]==1){
-                  return true;
-              }
-      }
-      path[node]=0;
-      check[node]=1;
-      return false;
-  }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int V=graph.size(); 
-        vector<int> adj[V];
-        for(int i=0;i<graph.size();i++){
-            for(int j=0;j<graph[i].size();j++){
-                adj[i].push_back(graph[i][j]);
+        int V = graph.size();
+        vector<int> adjRev[V];
+        vector<int> indegree(V,0);
+        for(int i=0; i<V; i++){
+            for(auto it:graph[i]){
+                adjRev[it].push_back(i);
+                indegree[i]++;
             }
         }
-        vector<int> visited(V,0);
-        vector<int> path(V,0);
-        vector<int> check(V,0);
-        for(int i=0;i<V;i++){
-            if(visited[i]==0){
-                dfs(i,path,visited,check,adj);
+        queue<int> q;
+        vector<int> v;
+        for(int i=0; i<V; i++){
+            if(indegree[i]==0){
+                q.push(i);
             }
         }
-        vector<int> ans;
-        for(int i=0;i<V;i++){
-            if(check[i]==1){
-                ans.push_back(i);
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            v.push_back(node);
+            for(auto it:adjRev[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
             }
         }
-        return ans;
+        sort(v.begin(), v.end());
+        return v;
     }
 };
