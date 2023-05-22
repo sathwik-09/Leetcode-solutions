39d3a57d88f45c97792778b664c0e7a3627ca36d
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-05-22 12:03:58 +0530

    Time: 100 ms (60.18%), Space: 16.9 MB (6.72%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,35 +1,48 @@
 class Solution {
-public:
-    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>> distance(n, vector<int>(n,1e7));
-        for(auto it:edges){
-            distance[it[0]][it[1]] = it[2];
-            distance[it[1]][it[0]] = it[2];
-        }
-        for(int i=0; i<n; i++){
-           distance[i][i]=0; 
-        }  
-        for(int k=0; k<n; k++){
-	        for(int i=0; i<n; i++){
-	            for(int j=0; j<n; j++){
-	                distance[i][j] = min(distance[i][j],distance[i][k]+distance[k][j]);
-	            }
-	        }
-	    }
-	    int nc = n;
-        int city = 0;
-        for(int i=0;i<n;i++){
-            int count = 0;
-            for(int j=0;j<n;j++){
-                if(distance[i][j]<=distanceThreshold){
-                    count++;
+private:
+    vector<int> dijkstra(vector<pair<int, int>> adj[], int n, int src) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> pq;
+        vector<int> temp(n, INT_MAX);
+        temp[src] = 0;
+        pq.push({0, src});
+        while(!pq.empty()) {
+            int prevNode = pq.top().second;
+            int prevDist = pq.top().first;
+            pq.pop();
+            for(auto it:adj[prevNode]) {
+                int currNode = it.first;
+                int currDist = it.second;
+                if(currDist + prevDist < temp[currNode]) {
+                    temp[currNode] = currDist+prevDist;
+                    pq.push({temp[currNode], currNode});
                 }
             }
-            if(count<=nc){
-                city = i;
-                nc = count;
+        }
+        return temp;
+    }
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<pair<int, int>> adj[n];
+        vector<vector<int>> dist(n, vector<int> (n, 1e9));
+        for(auto it:edges) {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+        for(int i=0; i<n; i++) {
+            dist[i] = dijkstra(adj, n, i);
+        }
+        int cityNo = -1;
+        int cityCnt = n;
+        for(int i=0; i<n; i++) {
+            int cnt = 0;
+            for(int j=0; j<n; j++) {
+                if(dist[i][j] <= distanceThreshold) cnt++;
+            }
+            if(cityCnt >= cnt) {
+                cityCnt = cnt;
+                cityNo = i;
             }
         }
-        return city;
+        return cityNo;
     }
 };
