0482430229ea3f24e8bc6c0f2d7b0d93584802da
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-05-25 00:57:40 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (36.86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
+    int solve(int n,vector<int> &dp){
+        if(n<=2)
+          return n;
+        if(dp[n]!=-1) 
+          return dp[n]; 
+        dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        return dp[n];
+    }
     int climbStairs(int n) {
         if(n<=2)
          return n;
-        vector<int> dp(n+1);
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
-        for(int i=3;i<=n;i++)
-         dp[i]=dp[i-1]+dp[i-2];
+        vector<int> dp(n+1,-1);
         
-        return dp[n];
+        return solve(n,dp);
     }
 };
