cdb48b591c9c10cb484fc1a69105fcd6d53f3957
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-05-25 23:45:24 +0530

    Time: 3 ms (93.94%), Space: 13.6 MB (77.88%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int solve(int index, vector<int>& cost,vector<int> &dp){
         if(index<0) return 0;
         if(index==0 || index==1){
@@ -8,7 +9,9 @@ public:
         if(dp[index]!=-1) return dp[index];
         return dp[index]=cost[index] + min(solve(index-1,cost,dp),solve(index-2,cost,dp));
     }
+    */
     int minCostClimbingStairs(vector<int>& cost) {
+        /*
         int n = cost.size();
         vector<int> dp(n,-1);
         dp[0] = cost[0];
@@ -18,5 +21,15 @@ public:
             
         }
         return min(dp[n-1],dp[n-2]);
+        */
+        int n = cost.size();
+        int first = cost[0];
+        int second = cost[1];
+        for(int i=2; i<n; i++){
+            int curr = cost[i] + min(first,second);
+            first = second;
+            second = curr;
+        }
+        return min(first,second);
     }
 };
