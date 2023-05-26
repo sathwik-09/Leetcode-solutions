3e676596a64c77f2fbb6fb5872808d594147d600
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-05-26 10:52:05 +0530

    Time: 5 ms (18.34%), Space: 6.6 MB (28.62%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-   
+   /* 
     int solve(int i, int j,vector<vector<int>> &dp){
         if(i==0 && j==0) return 1;
         if(i<0 || j<0) return 0;
@@ -10,9 +10,15 @@ public:
         int ans = up + left;
         return dp[i][j]=ans;
     }
+    */
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,1e9));
-        return solve(m-1,n-1,dp);
+        vector<vector<int>> dp(m,vector<int>(n,1));
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
         
     }
 };
