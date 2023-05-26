3b9304bfa620efe5e850514235f800141ed51398
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-05-26 17:30:17 +0530

    Time: 12 ms (13.91%), Space: 8.6 MB (90.84%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -11,6 +11,7 @@ public:
     */
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
+        /*
         vector<vector<int>> dp(n,vector<int>(n,0));
         for(int j=0; j<n; j++){
             dp[n-1][j] = triangle[n-1][j];
@@ -23,5 +24,19 @@ public:
             }
         }
         return dp[0][0];
+        */
+        vector<int> prev(n,0),curr(n,0);
+        for(int j=0; j<n; j++){
+            prev[j] = triangle[n-1][j];
+        }
+        for(int i=n-2; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                int down = triangle[i][j] + prev[j];
+                int dig= triangle[i][j] + prev[j+1]; 
+                curr[j] = min(down,dig);
+            }
+            prev = curr;
+        }
+        return prev[0];
     }
 };
