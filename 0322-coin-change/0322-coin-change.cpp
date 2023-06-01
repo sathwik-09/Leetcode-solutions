934defa2a9b681dda9f2d1ad62430c819667b7ef
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-06-01 11:15:08 +0530

    Time: 138 ms (36.44%), Space: 18.7 MB (39.64%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int solve(int index, vector<int>& coins, int amount, vector<vector<int>> &dp){
         if(index==0){
             if(amount%coins[index]==0) return amount/coins[0];
@@ -11,25 +12,29 @@ public:
         if(coins[index]<=amount) include = 1 + solve(index,coins,amount-coins[index],dp);
         return dp[index][amount]=min(include,exclude);
     }
+    */
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        vector<int> prev(amount+1,0), curr(amount+1,0);
         for(int t=0; t<=amount; t++){
             if(t%coins[0]==0){
-                dp[0][t]=t/coins[0];
+                prev[t]=t/coins[0];
             }
-            else dp[0][t]=1e9;
+            else prev[t]=1e9;
         }
         for(int index=1; index<n; index++){
             for(int t=0; t<=amount; t++){
-                int exclude = 0 + dp[index-1][t];
+                int exclude = 0 + prev[t];
                 int include = INT_MAX;
-                if(coins[index]<=t) include = 1 + dp[index][t-coins[index]];
-                dp[index][t]=min(include,exclude);
+                if(coins[index]<=t){
+                    include = 1 + curr[t-coins[index]];
+                } 
+                curr[t]=min(include,exclude);
             }
+            prev=curr;
             
         }
-        int ans = dp[n-1][amount];
+        int ans = prev[amount];
         if(ans!=1e9) return ans;
         else return -1;
     }
