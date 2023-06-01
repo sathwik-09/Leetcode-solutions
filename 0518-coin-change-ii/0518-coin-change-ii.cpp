8009ddb63c887bf44c4c0f1e54a301be60223ae0
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-06-01 12:27:17 +0530

    Time: 28 ms (53.49%), Space: 7.4 MB (79.39%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -14,18 +14,19 @@ public:
     */
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        vector<int> prev(amount+1,0), curr(amount+1,0);
         for(int i=0; i<=amount; i++){
-            dp[0][i] = (i%coins[0]==0) ;
+            prev[i] = (i%coins[0]==0) ;
         }
         for(int index=1; index<n; index++){
             for(int target=0; target<=amount; target++){
-                long long exclude = dp[index-1][target];
+                long long exclude = prev[target];
                 long long include = 0;
-                if(coins[index]<=target) include = dp[index][target-coins[index]];
-                dp[index][target]=include+exclude;
+                if(coins[index]<=target) include = curr[target-coins[index]];
+                curr[target]=include+exclude;
             }
+            prev=curr;
         }
-        return dp[n-1][amount];
+        return prev[amount];
     }
 };
