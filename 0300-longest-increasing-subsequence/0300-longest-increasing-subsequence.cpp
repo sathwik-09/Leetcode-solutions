444a0d935a64d1b379971d5244efae2e6d047792
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-07-22 11:49:53 +0530

    Time: 14 ms (82.64%), Space: 10.3 MB (96.89%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -10,7 +10,6 @@ public:
         }
         return dp[ind][prev_ind+1]=max(take,notTake);
     }
-    */
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> dp(n+1, vector<int>(n+1,0));
@@ -24,5 +23,23 @@ public:
             }
         }
         return dp[0][0];
+    }
+    */
+    int lengthOfLIS(vector<int>& nums){
+        int n = nums.size();
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        int len = 1;
+        for(int i=1; i<n; i++){
+            if(nums[i]>temp.back()){
+                temp.push_back(nums[i]);
+                len++;
+            }
+            else{
+                int ind = lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                temp[ind]=nums[i];
+            }
+        }
+        return len;
     }
 };
