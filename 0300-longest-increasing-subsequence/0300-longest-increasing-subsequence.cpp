61b471578cbdac827887008fcd436e3822d27343
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-07-22 10:33:18 +0530

    Time: 505 ms (42.83%), Space: 10.5 MB (51.82%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int solve(int ind, int prev_ind, int n, vector<int>& nums, vector<vector<int>> &dp){
         if(ind==n) return 0;
         if(dp[ind][prev_ind+1]!=-1) return dp[ind][prev_ind+1];
@@ -9,9 +10,19 @@ public:
         }
         return dp[ind][prev_ind+1]=max(take,notTake);
     }
+    */
     int lengthOfLIS(vector<int>& nums) {
+        int ans = 1;
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return solve(0,-1,n,nums,dp);
+        vector<int> dp(n, 1);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[i] > nums[j]) {
+				    dp[i] = max(dp[i], dp[j] + 1);
+                    ans = max(ans, dp[i]);
+                }
+            }
+        }
+        return ans;
     }
 };
