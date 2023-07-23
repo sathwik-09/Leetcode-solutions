29c708cb1ab0d738efac89e2d8c1dcf202578302
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-07-23 23:21:07 +0530

    Time: 14 ms (85.22%), Space: 8.5 MB (80.97%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int solve(int ind, int n, vector<int>& arr, int k,vector<int> &dp){
         if(ind==n) return 0;
         if(dp[ind]!=-1) return dp[ind];
@@ -12,9 +13,20 @@ public:
         }
         return dp[ind]=maxAns;
     }
+    */
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> dp(n+1,-1);
-        return solve(0,n,arr,k,dp);
+        vector<int> dp(n+1,0);
+        for(int ind=n-1; ind>=0; ind--){
+            int len=0,maxi=INT_MIN,maxAns=INT_MIN;
+            for(int j=ind; j<min(n,ind+k); j++){
+                len++;
+                maxi = max(maxi,arr[j]);;
+                int sum = (len*maxi)+dp[j+1];
+                maxAns = max(maxAns,sum);
+            }
+            dp[ind]=maxAns;
+        }
+        return dp[0];
     }
 };
