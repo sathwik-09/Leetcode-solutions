0bdb3cf1c581823844381e7a0e00be745cd9bb98
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-07-27 20:41:59 +0530

    Time: 126 ms (77.51%), Space: 67 MB (38.96%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -8,13 +8,13 @@ public:
         for(int i=0; i<n; i++){
             st.insert(nums[i]);
         }
-        for(auto it : st){
-            if(st.find(it-1) == st.end()){
+        for(auto it:st){
+            if(st.find(it-1)==st.end()){
                 int cnt=1;
                 int x = it;
-                while(st.find(x+1) != st.end()){
+                while(st.find(x+1)!=st.end()){
                     x=x+1;
-                    cnt = cnt + 1;
+                    cnt+=1;
                 }
                 maxi = max(maxi,cnt);
             }
