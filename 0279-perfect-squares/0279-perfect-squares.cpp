7fa288b7b5eff3ca7e325769f014742657b40139
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-07-30 13:27:25 +0530

    Time: 153 ms (82.32%), Space: 9.1 MB (70.99%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int solve(int i, int n,vector<int> &dp){
         if(n==0) return 0;
         if(dp[n]!=-1) return dp[n];
@@ -8,8 +9,16 @@ public:
         }
         return dp[n]=1e5;
     }
+    */
     int numSquares(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(1,n,dp);
+        vector<int>dp(n+1, 1e5);
+        dp[0]=0;
+        int j=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                dp[i]=min(1+dp[i-j*j], dp[i]);
+            }
+        }
+        return dp[n];
     }
 };
