646c9a8a1fd5118741eaa2e4e0424670be109cf9
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-07-30 12:58:21 +0530

    Time: 3 ms (97.56%), Space: 8.6 MB (94.42%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int solve(int i, int j,int m,int n,vector<vector<int>>& dungeon,vector<vector<int>> &dp){
         if(i >= m+1 or j >= n+1) return 1e9;
         if(i==m && j==n){
@@ -18,5 +19,23 @@ public:
         int n = dungeon[0].size();
         vector<vector<int>> dp(m,vector<int>(n,-1));
         return solve(0,0,m-1,n-1,dungeon,dp);
+    }
+    */
+    int calculateMinimumHP(vector<vector<int>>& dungeon){
+        int n = dungeon.size(), m = dungeon[0].size();
+        vector<int> dp(m+1,1e9);
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(i == n-1 and j==m-1){
+                    dp[j] = dungeon[i][j] > 0 ? 1 : 1 - dungeon[i][j];
+                    continue;
+                }
+                int down = dp[j];
+                int right = dp[j+1];
+                int res = min(down,right) - dungeon[i][j];
+                dp[j] = res > 0 ? res : 1;
+            }
+        }        
+        return dp[0];
     }
 };
