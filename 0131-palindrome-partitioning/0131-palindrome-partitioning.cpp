d36cf899c1b388fed9345a8403ffe70890877a4d
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-08-05 17:45:47 +0530

    Time: 105 ms (86.62%), Space: 57.7 MB (36.41%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    bool isPalindrome(string s,int start, int end){
+        while(start<=end){
+            if(s[start++]!=s[end--]) return false;
+        }
+        return true;
+    }
+    void solve(int index,string s,vector<vector<string>> &ans,vector<string> &temp){
+        if(index==s.size()){
+            ans.push_back(temp);
+            return;
+        }
+        for(int i=index; i<s.size(); i++){
+            if(isPalindrome(s,index,i)){
+                temp.push_back(s.substr(index,i-index+1));
+                solve(i+1,s,ans,temp);
+                temp.pop_back();
+            }
+        }
+    }
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> temp;
+        solve(0,s,ans,temp);
+        return ans;
+    }
+};
