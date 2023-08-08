01fe93e30dd245efc533e33871efcb0bff0c00d2
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-08-08 21:41:04 +0530

    Time: 3 ms (76.59%), Space: 11.1 MB (13.70%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,20 +6,15 @@ public:
         int e = n-1;
         int mid=s + (e-s)/2;
         while(s<=e){
-            mid = (s+e)/2;
-            if(nums[mid]==target)
-                return mid;
+            mid = s+(e-s)/2;
+            if(nums[mid]==target) return mid;
             else if(nums[mid]>=nums[s]){
-                if(target<=nums[mid] && target>=nums[s])
-                    e = mid-1;
-                else
-                    s = mid+1;
+                if(target<=nums[mid] && target>=nums[s]) e = mid-1;
+                else s = mid+1;
             }
             else{
-                if(target>=nums[mid] && target<=nums[e])
-                    s = mid+1;
-                else
-                    e = mid-1;
+                if(target>=nums[mid] && target<=nums[e]) s = mid+1;
+                else e = mid-1;
             }
         }
         return -1;
