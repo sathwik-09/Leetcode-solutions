a9efb49a217518479ed85782151937efd2f610e7
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2023-08-11 23:32:50 +0530

    Time: 26 ms (57.25%), Space: 7.4 MB (81.57%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,17 +1,5 @@
 class Solution {
 public:
-    /*
-    int solve(int index, vector<int>& coins, int amount,vector<vector<int>> dp){
-        if(index==0){
-            return (amount%coins[index]==0) ;
-        }
-        if(dp[index][amount]!=-1) return dp[index][amount];
-        int exclude = solve(index-1, coins, amount,dp);
-        int include = 0;
-        if(coins[index]<=amount) include = solve(index,coins,amount-coins[index],dp);
-        return dp[index][amount]=include+exclude;
-    }
-    */
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<int> prev(amount+1,0), curr(amount+1,0);
