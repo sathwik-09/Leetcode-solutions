1184cca25293d823980c406e1eea4a1ac1af8285
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2024-01-02 22:38:07 +0530

    Time: 32 ms (5.35%), Space: 30.7 MB (15.92%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     vector<vector<int>> findMatrix(vector<int>& nums) {
         vector<vector<int>> ans;
         unordered_map<int,int> mpp;
@@ -9,6 +10,29 @@ public:
                 ans.push_back({});
             }
             ans[mpp[el]-1].push_back(el);
+        }
+        return ans;
+    }
+    */
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        vector<vector<int>> ans;
+        unordered_map<int,int> mpp;
+        int maxi=0;
+        for(int i=0; i<nums.size(); i++){
+            mpp[nums[i]]++;
+        }
+        for(auto i:mpp){
+            maxi=max(maxi,i.second);
+        }
+        for(int i=0; i<maxi; i++){
+            vector<int> temp;
+            for(auto& i:mpp){
+                if(i.second>0){
+                    temp.push_back(i.first);
+                    i.second--;
+                }
+            }
+            ans.push_back(temp);
         }
         return ans;
     }
