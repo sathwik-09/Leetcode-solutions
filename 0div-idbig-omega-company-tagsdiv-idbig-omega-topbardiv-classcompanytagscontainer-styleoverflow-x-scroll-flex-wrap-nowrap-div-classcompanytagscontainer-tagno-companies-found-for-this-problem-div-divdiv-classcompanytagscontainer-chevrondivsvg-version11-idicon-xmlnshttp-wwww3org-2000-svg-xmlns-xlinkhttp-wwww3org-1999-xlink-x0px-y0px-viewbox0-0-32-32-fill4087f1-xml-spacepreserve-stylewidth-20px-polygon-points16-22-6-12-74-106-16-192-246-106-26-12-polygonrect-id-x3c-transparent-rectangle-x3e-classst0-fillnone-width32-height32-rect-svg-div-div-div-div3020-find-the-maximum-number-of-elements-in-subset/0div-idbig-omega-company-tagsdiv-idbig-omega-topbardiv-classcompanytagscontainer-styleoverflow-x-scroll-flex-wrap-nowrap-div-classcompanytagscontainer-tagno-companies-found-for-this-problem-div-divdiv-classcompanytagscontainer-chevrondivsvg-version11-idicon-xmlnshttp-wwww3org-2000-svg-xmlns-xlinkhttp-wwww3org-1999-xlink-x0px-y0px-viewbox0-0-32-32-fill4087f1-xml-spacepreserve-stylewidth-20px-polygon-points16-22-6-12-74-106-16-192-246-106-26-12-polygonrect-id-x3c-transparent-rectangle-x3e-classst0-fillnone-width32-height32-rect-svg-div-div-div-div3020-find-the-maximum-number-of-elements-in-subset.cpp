7252c6c2a94eb2706399389a4780b9d422cc861e
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2024-01-30 11:44:35 +0530

    Time: 374 ms (16.32%), Space: 125.9 MB (46.55%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3020-find-the-maximum-number-of-elements-in-subset/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3020-find-the-maximum-number-of-elements-in-subset.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3020-find-the-maximum-number-of-elements-in-subset/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3020-find-the-maximum-number-of-elements-in-subset.cpp
@@ -2,33 +2,30 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        int ans=0;
-        // used map for sorting the frequencies to figure out that lowest numbers can be taken first
-        map<ll , int> mpp;
-        for(int i=0; i<nums.size(); i++){
-            mpp[nums[i]]++;
-        }
-        for(auto [el,freq]:mpp){
-            ll x=el;
-            int sz=0;
-            while(mpp.find(x)!=mpp.end() && mpp[x]){
-                // edge case if elememnt=1 then add all the occurences of 1
-                if(x==1) sz+=mpp[1];
-                else if(mpp[x]>=2) sz+=2;
-                // if freq of el is 1 then simply increase cnt and break cause there will no more occurences
-                else if(mpp[x]==1){
-                    sz+=1;
+        long long n = nums.size();
+        map<long long, long long> mp;
+        for(auto i: nums) mp[i]++;
+        sort(nums.begin(), nums.end());
+        long long mx = 1;
+        for(int i=0; i<n; i++){
+            long long cur = nums[i];
+            long long cur_max = 0;
+            while(mp[cur]){
+                if(mp[cur] < 2){
+                    cur_max += mp[cur];
+                    mp[cur] = 0;
                     break;
                 }
-                // after adding occurences make it zero as those will be no use
-                mpp[x]=0;
-                if(x>1e5) break;
-                //for the next pass it keeps on increasing by raising of power 2
-                x=x*x;
+                cur_max += 2;
+                mp[cur] -= 2;
+                cur *= cur;
             }
-            //If answer is even then subract 1 as size would be odd means deleting the max number which occured twice
-            ans=max(ans,sz-(sz%2==0));
+            if(cur_max % 2 == 0){
+                mx = max(mx, cur_max - 1);
+            }
+            else mx = max(mx, cur_max);
         }
-        return ans;
+        
+        return mx;
     }
 };
