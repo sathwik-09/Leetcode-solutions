3ee4cab5bae173cdcf3ecbf9606f413f49434641
Enapothula Sathvik <98477494+sathwik-09@users.noreply.github.com>
2024-01-30 11:48:03 +0530

    Time: 283 ms (53.45%), Space: 118.7 MB (74.41%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3020-find-the-maximum-number-of-elements-in-subset/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3020-find-the-maximum-number-of-elements-in-subset.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3020-find-the-maximum-number-of-elements-in-subset/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3020-find-the-maximum-number-of-elements-in-subset.cpp
@@ -2,6 +2,36 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
+        int ans=0;
+        // used map for sorting the frequencies to figure out that lowest numbers can be taken first
+        map<ll , int> mpp;
+        for(int i=0; i<nums.size(); i++){
+            mpp[nums[i]]++;
+        }
+        for(auto [el,freq]:mpp){
+            ll x=el;
+            int sz=0;
+            while(mpp.find(x)!=mpp.end() && mpp[x]){
+                if(mpp[x] < 2){
+                    sz += mpp[x];
+                    mpp[x] = 0;
+                    break;
+                }
+                sz += 2;
+                mpp[x] -= 2;
+                x *= x;
+            }
+            //If answer is even then subract 1 as size would be odd means deleting the max number which occured twice
+            ans=max(ans,sz-(sz%2==0));
+        }
+        return ans;
+    }
+};
+
+
+
+/*
+int maximumLength(vector<int>& nums) {
         long long n = nums.size();
         map<long long, long long> mp;
         for(auto i: nums) mp[i]++;
@@ -27,5 +57,4 @@ public:
         }
         
         return mx;
-    }
-};
+        */
